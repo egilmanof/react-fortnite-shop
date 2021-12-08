b0d163d0af0ca390a45a55cab21531ab4a1b5c05
Eduard Gilmanov <mailnox@mail.ru>
2021-12-08 17:57:24 +0500

    refactor code

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
                 <div className='footer_copy'>
                     Сopyright &copy;
                 </div>
-                <a href="https://github.com/egilmanof" target='_blank' className='footer_link'>GitHub автора</a>
+                <a href="https://github.com/egilmanof"  className='footer_link'>GitHub автора</a>
                 <div className="footer_data">
                     {(new Date()).getFullYear()}г.
                 </div>

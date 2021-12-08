eb04bc196ef06a7857330038ef1ea4f1dda66ab6
Eduard Gilmanov <mailnox@mail.ru>
2021-12-08 20:58:41 +0500

    refactor code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 
-import {BrowserRouter as Router} from "react-router-dom";
-import AppRouter from "./components/AppRouter";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import News from "./pages/News";
+import Sale from "./pages/Sale";
+import Feedback from "./pages/Feedback";
+import Notfound from "./pages/Notfound";
 
 function App() {
     return (
         <Router>
-            <AppRouter/>
+            <Routes>
+                <Route path='/' element={<News/>} />
+                <Route path='react-fortnite-shop' element={<News/>} />
+                <Route path='/sale' element={<Sale/>} />
+                <Route path='/feedback' element={<Feedback/>} />
+                <Route path='*' element={<Notfound/>} />
+            </Routes>
         </Router>
     );
 }
